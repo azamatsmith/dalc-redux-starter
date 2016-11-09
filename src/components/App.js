5aeeef61b5f115f1868e7a6685779d7b47f072d5
Matthew Smith <azamatsmith@gmail.com>
2016-11-09 10:58:59 -0700

    Updated header

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-				<h2 className="text-center">DALC-REDUX-STARTER</h2>
+				<h2 className="text-center">DALC Redux Starter</h2>
 				<Landing />
       </div>
     );
